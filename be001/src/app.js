0d7bd5ba68122651c08590ac568c1d26e07791f7
Hoang Do <me@kaid.sh>
2019-06-22 17:40:26 +0700

    BE001: Use DB for Products controller

--- be001/src/app.js
+++ be001/src/app.js
@@ -3,7 +3,11 @@ import bodyParser from 'body-parser';
 import productsRoutes from './routes/products';
 
 const app = express();
+
+// middlewares
 app.use(bodyParser.json());
+
+// register routes
 app.use('/products', productsRoutes);
 
 export default app;
--- be001/src/controllers/products.js
+++ be001/src/controllers/products.js
@@ -1,82 +1,81 @@
-const products = [
-  {
-    id: 1,
-    name: 'Test product 1',
-    description: 'A test product',
-    price: 10000,
-  },
-  {
-    id: 2,
-    name: 'Test product 2',
-    description: 'Another test product',
-    price: 5000,
-  },
-];
+import models from '../models';
 
-let productsCounter = products.length;
+const { Product } = models;
 
-export const index = (req, res, next) => {
-  res.json(products);
-  next();
-};
-
-export const show = (req, res, next) => {
-  const id = parseInt(req.params.id, 10);
-  const product = products.find(p => p.id === id);
-  if (product) {
-    res.json(product);
-    next();
-  } else {
-    next(new Error('Product not found'));
+export const index = async (req, res, next) => {
+  try {
+    const products = await Product.findAll();
+    res.json(products);
+  } catch (err) {
+    next(err);
   }
 };
 
-export const create = (req, res, next) => {
-  productsCounter += 1;
-
-  const { name, description, price } = req.body;
-  const newProduct = {
-    id: productsCounter,
-    name,
-    description,
-    price,
-  };
-  products.push(newProduct);
-
-  res.status(201);
-  res.json(newProduct);
-  next();
+export const show = async (req, res, next) => {
+  try {
+    const id = parseInt(req.params.id, 10);
+    const product = await Product.findByPk(id);
+    if (product) {
+      res.json(product);
+    } else {
+      next(new Error('Product not found'));
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const update = (req, res, next) => {
-  const id = parseInt(req.params.id, 10);
-  const product = products.find(p => p.id === id);
-  if (product) {
+export const create = async (req, res, next) => {
+  try {
     const { name, description, price } = req.body;
-    if (name) {
-      product.name = name;
-    }
-    if (description) {
-      product.description = description;
-    }
-    if (price) {
-      product.price = price;
-    }
-    res.json(product);
-    next();
-  } else {
-    next(new Error('Product not found'));
+    const newProduct = await Product.create({
+      name,
+      description,
+      price,
+    });
+    res.status(201).json(newProduct);
+  } catch (err) {
+    next(err);
   }
 };
 
-export const destroy = (req, res, next) => {
-  const id = parseInt(req.params.id, 10);
-  const idx = products.findIndex(p => p.id === id);
-  if (idx > -1) {
-    const deleted = products.splice(idx, 1)[0];
-    res.json(deleted);
-    next();
-  } else {
-    next(new Error('Product not found'));
+export const update = async (req, res, next) => {
+  try {
+    const id = parseInt(req.params.id, 10);
+    const { name, description, price } = req.body;
+    const [rowsUpdated] = await Product.update({
+      name,
+      description,
+      price,
+    }, {
+      where: {
+        id,
+      },
+    });
+    if (rowsUpdated === 1) {
+      res.status(204).end();
+    } else {
+      next(new Error('No record updated'));
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const destroy = async (req, res, next) => {
+  try {
+    const id = parseInt(req.params.id, 10);
+    const rowsDestroyed = await Product.destroy({
+      where: {
+        id,
+      },
+    });
+    if (rowsDestroyed === 1) {
+      res.status(204).end();
+    } else {
+      next(new Error('No record destroyed'));
+    }
+  } catch (err) {
+    next(err);
   }
 };
