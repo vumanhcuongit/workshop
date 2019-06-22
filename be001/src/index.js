57919fc592fbb772efda85f9245487c4cdc38037
Hoang Do <me@kaid.sh>
2019-06-22 15:10:44 +0700

    BE001: Setup Express server

--- be001/src/index.js
+++ be001/src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello world');
+import debug from 'debug';
+import app from './app';
+
+const dbg = debug('BE001:main');
+
+(async () => {
+  await app.listen(3000);
+  dbg('Server is listening on port 3000');
+})();
