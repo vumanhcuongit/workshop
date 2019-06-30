d0ba15530fb9d1e9fd26b934301bec5a921bf2cb
Hoang Do <me@kaid.sh>
2019-06-30 11:25:42 +0700

    BE001: Add more tests

--- be001/tests/controllers/products.test.js
+++ be001/tests/controllers/products.test.js
@@ -1,5 +1,14 @@
 import request from 'supertest';
 import app from '../../src/app';
+import models from '../../src/models';
+
+const { Product } = models;
+
+const sampleProductData = {
+  name: 'Sample Product',
+  description: 'A sample product for testing',
+  price: 9999,
+};
 
 describe('Products controller', () => {
   describe('#index', () => {
@@ -7,6 +16,47 @@ describe('Products controller', () => {
       const response = await request(app).get('/products').send();
       expect(response.statusCode).toBe(200);
       expect(response.body).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('#show', () => {
+    it('shows a product detals', async () => {
+      const sample = await Product.create(sampleProductData);
+      const response = await request(app).get(`/products/${sample.id}`).send();
+      expect(response.statusCode).toBe(200);
+      expect(response.body.id).toEqual(sample.id);
+      expect(response.body.name).toEqual(sample.name);
+      expect(response.body.description).toEqual(sample.description);
+      expect(response.body.price).toEqual(sample.price);
+    });
+  });
+
+  describe('#create', () => {
+    it('creates a product', async () => {
+      const response = await request(app).post('/products').send(sampleProductData);
+      expect(response.status).toBe(201);
+      expect(response.body.name).toEqual(sampleProductData.name);
+      expect(response.body.description).toEqual(sampleProductData.description);
+      expect(response.body.price).toEqual(sampleProductData.price);
+    });
+  });
+
+  describe('#update', () => {
+    it('updates a product detals', async () => {
+      const sample = await Product.create(sampleProductData);
+      const response = await request(app).put(`/products/${sample.id}`).send({
+        name: 'Updated name',
+      });
+      expect(response.statusCode).toBe(204);
+    });
+  });
+
+  describe('#destroy', () => {
+    it('deletes a product detals', async () => {
+      const sample = await Product.create(sampleProductData);
+      const sampleId = sample.id;
+      const response = await request(app).delete(`/products/${sampleId}`).send();
+      expect(response.statusCode).toBe(204);
     });
   });
 });
